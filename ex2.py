23ae6022a8833c0f327bd985dc79b43a0c591c3c
avi13s <59297204+avi13s@users.noreply.github.com>
2019-12-28 14:13:16 +0200

    Add files via upload

--- ex2.py
+++ ex2.py
@@ -16,8 +16,10 @@ class TileKB:
     def __init__(self, initial_type):
         self.KB_dict = defaultdict(lambda: False)
         self.been_at = 0  # counting how much times a hero stepped here, updates after moving
+        self.SAFE = False
         self.WALL = False
         self.GOLD = False
+        self.shot_to_dir = []
         if 11 <= initial_type <= 14:
             self.SAFE = True
         if initial_type == 20:
@@ -70,13 +72,22 @@ class WumpusController:
 
         # Timeout: 60 seconds
 
+    def should_i_shoot(self, curr_tile, direction):
+        if direction in self.map_dict[curr_tile].shot_to_dir:
+            return False
+        dir_tup = self.directions[direction]
+        if self.map_dict[t_add(dir_tup,curr_tile)].WALL or self.map_dict[t_add(dir_tup,curr_tile)].SAFE:
+            return False
+        return True
 
-    #  TODO : if stench - > shoot if didn't shoot already, if coor+dir != (WALL OR SAFE)
+    #  TODO : if stench - > shoot if didn't shoot already, if coor+dir != (WALL OR SAFE)  - Done
     #  more TODO: update SAFE tiles for heroes with no observations
     #  TODO glitter+SAFE+been_at ==0 => GO! (after checking if tile.GOLD == True)
+    #  TODO don't step on other heroes!
     def get_next_action(self, partial_map, observations):
+        #print(observations)
         if self.last_action is None:
-            curr_hero = random.choice(self.heroes.keys())
+            curr_hero = random.choice(list(self.heroes.keys()))
         else:
             curr_hero = self.last_action[1]
             if partial_map(self.heroes[curr_hero]) != curr_hero:  # means the hero is dead
@@ -85,17 +96,23 @@ class WumpusController:
         for observation in observations:  # updating our KB
             coordinate = observation[0]
             obs = observation[1]
-            hero = partial_map[coordinate[0]][coordinate[1]]
+            hero = partial_map[coordinate[0]-1][coordinate[1]-1]
             last_dir = self.last_direction[hero]
-            if self.last_action != ('shoot', hero, last_dir):
-
+            if obs == 'stench':
+                for direction in self.directions.keys():
+                    if opposite_direction(direction) == last_dir:
+                        continue
+                    else:
+                        if self.should_i_shoot(coordinate, direction):
+                            self.map_dict[coordinate].shot_to_dir.append(direction)
+                            print(f"i did {('shoot', hero, direction)}")
+                            return 'shoot', hero, direction
             for direction in self.directions:
                 self.map_dict[t_add(coordinate, self.directions[direction])].update_after_obs(opposite_direction(direction), obs)
-
-
-
-        self.prev_map = partial_map
-        return 'move', 11, 'R'
+        #self.prev_map = partial_map   - probably don't need
+        self.heroes[curr_hero] = t_add(self.heroes[curr_hero], self.directions['R'])
+        print(f"i did {('move', 11, 'R')}")
+        return 'move', 11, 'R'  # just default to see how it runs
         # TODO: before returning next, let's update been_at for the KBs and "came_from"
         # Timeout: 5 seconds
 
